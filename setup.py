6c3e285461d7952ddc1120d7048a90d428f29387
Greg 'gba' Albrecht Kim <gba@splunk.com>
2012-07-20 20:56:09 -0700

    1.0.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read_readme():
 
 setuptools.setup(
     name='pypirc',
-    version='1.0.2',
+    version='1.0.3',
     description='PyPiRC: .pypirc Manager',
     long_description=read_readme(),
     author='Greg Albrecht',
